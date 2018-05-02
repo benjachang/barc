546d9622eceeed31edc7a92ac0263da07b0b2b08
jgonzal13 <jon.gonzales@berkeley.edu>
2018-05-02 22:11:24 +0100

    changed read/write permissions

--- scripts/team_name.sh
+++ scripts/team_name.sh
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
+#<<<<<<< HEAD
 export TEAM_NAME="ME131-Rebecca"
-=======
+# =======
 export TEAM_NAME="MPC_BARC_2"
->>>>>>> 913163a1acddc1d846eb269c04ae3dc60ecbc2bd
+#>>>>>>> 913163a1acddc1d846eb269c04ae3dc60ecbc2bd
