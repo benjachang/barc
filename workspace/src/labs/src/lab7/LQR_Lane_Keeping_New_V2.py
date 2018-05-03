e6eac4c26cc3edec1a28da88059a4685f07875b3
Ben Chang <benjachang12@berkley.edu>
2018-05-02 21:18:55 -0700

    tuned gains

--- workspace/src/labs/src/lab7/LQR_Lane_Keeping_New_V2.py
+++ workspace/src/labs/src/lab7/LQR_Lane_Keeping_New_V2.py
@@ -35,7 +35,7 @@ class image_processing_node():
         self.h = 480
 
         # Reference velocity
-        self.v_ref = 1.3
+        self.v_ref = 1.2
 
         # Number of moving average points
         self.sx = 5
@@ -404,7 +404,7 @@ class image_processing_node():
                 dt = self.dt
                 lr = 0.15
                 lf = 0.15
-                j = 2
+                j = 1
                 interval = 2
                 x_ref_for_radius = [x_ref[j+interval],x_ref[j+interval*2]]
                 y_ref_for_radius = [y_ref[j+interval],y_ref[j+interval*2]]
@@ -443,7 +443,7 @@ class image_processing_node():
                 print('beta_des',beta_des)
                 print('psi_des',psi_des*180/pi)
                 """
-		print('psi_des',psi_des*180/pi)
+				
                 if j == 0:
                     z = np.matrix([[0],[0],[0]])
                 else:
@@ -456,8 +456,8 @@ class image_processing_node():
                 Bc = np.matrix([[cos(psi_des + beta_des),(-v_ref*sin(psi_des + beta_des))],[sin(psi_des + beta_des),(v_ref*cos(psi_des + beta_des))],[(sin(beta_des)/lr),(v_ref*cos(beta_des)/lr)]])
 
                 ### TUNE THESE LQR GAINS ###
-                Q = np.matrix([[50, 0, 0],[0, 100, 0],[0, 0, 1]]);
-                R = np.matrix([[25, 0 ],[0, 1]]);
+                Q = np.matrix([[200, 0, 0],[0, 300, 0],[0, 0, 100]]);
+                R = np.matrix([[25, 0 ],[0, 10]]);
 
                 """ These are the starting gains from the solutions
                 Q = np.matrix([[50, 0, 0],[0, 50, 0],[0, 0, 1]]);
@@ -487,6 +487,10 @@ class image_processing_node():
                     #print(np.mean(self.movmean,axis=1))
                     vOpt = np.mean(self.movmean,axis=1)[0]
                     deltaOpt = np.mean(self.movmean,axis=1)[1]
+                
+                print('Radius',Radius)
+                print('vopt ',vOpt)
+                print('deltaOpt ',deltaOpt)
 
                 """
                 print('z_ref ',z_ref)
